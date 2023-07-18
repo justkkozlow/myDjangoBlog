658017c3a5961192a2c8de6fcbe8f52a8d7f6ad5
Kirill Kozlow <kozlov.khv@gmail.com>
2023-07-19 07:26:57 +1000

    Add functional Email share form

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,6 +126,14 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = 'kozlov.khv@gmail.com'
+EMAIL_HOST_PASSWORD = 'mykvcwsripytnrgt'
+
 JAZZMIN_SETTINGS = {
     'changeform_format': 'horizontal_tabs',
     'changeform_format_overrides': {'auth.user': 'collapsible', 'auth.group': 'vertical_tabs'},
