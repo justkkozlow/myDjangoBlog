28c4af1312c872f68b0ee1859cde1aaec25ab800
Kozlow <kozlov.khv@gmail.com>
2023-07-20 13:37:12 +1000

    Add markdown to create and edit posts

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,12 +1,24 @@
 from django import template
+from django.db.models import Count
+from django.utils.safestring import mark_safe
+
 from ..models import Post
+
+import markdown
+
 
 register = template.Library()
 
 
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
+
+
 @register.simple_tag
-def total_posts():
-    return Post.published.count()
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(
+        total_comments=Count('comments')).order_by('-total_comments')[:count]
 
 
 @register.inclusion_tag('blog/post/latest_posts.html')
