626f0efefd26f5684915499141f8df4a16ad22cd
KIRILL <36366596+justkkozlow@users.noreply.github.com>
2023-07-21 14:23:09 +1000

    Update models.py
    Some visual changing

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,6 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 from taggit.managers import TaggableManager
-from mptt.models import MPTTModel, TreeForeignKey
 
 
 class PublishedManager(models.Manager):
